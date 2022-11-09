782a40ba684d33db113e9f911e66c8c2ad352a28
Andremandola <111663338+Andremandola@users.noreply.github.com>
2022-11-09 18:05:34 -0300

    repetições

---
+++ aula13/ambiente.js
@@ -0,0 +1,21 @@
+var C = 1
+do{
+    console.log('Tudo bem?')
+    C++
+} while(C <=6)
+
+/* var C = 1
+while (C <= 500){
+    console.log('Tudo bem?')
+    C++
+}
+*/
+
+/* 
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
