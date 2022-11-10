4712ed5ca0432a6371d687219033ca311220796e
Andremandola <111663338+Andremandola@users.noreply.github.com>
2022-11-10 16:03:26 -0300

    vetor

--- aula015/ambiente.js
+++ aula015/ambiente.js
@@ -1,3 +1,12 @@
+/* 
 let num =[5, 8, 2, 9, 3]
 console.log(`Nosso vetor é 0 ${num}`)
 num[3]= 6
+*/
+
+let num =[5, 8, 2, 9, 3]
+num.sort()
+console.log(num)
+console.log(`O vetor tem ${num.length} posições`)
+console.log(num[0])
+
