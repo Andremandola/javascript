c04bc517334dea9ddb2b5823039b3033858745e4
Andremandola <111663338+Andremandola@users.noreply.github.com>
2022-11-11 16:32:09 -0300

    teste hora

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -5,9 +5,18 @@ var img = window.document.getElementById('imagem')
 var data = new Date()
 var hora = data.getHours()
 msg.innerHTML = `Agora são ${hora} horas.`
-if (hora >= 0 && hora < 12) {
-    //Bom dia!
-} else if (hora >= 12 && hora < 18) {
-    //BOA TARDE!
-} 
 
+    if (hora >= 0 && hora < 12) {
+        //Bom DIA!
+        img.src ='manha.jpg' 
+        document.body.style.background = '#e2cd9f'
+    } else if (hora >= 12 && hora < 18) {
+        //BOA TARDE!
+        img.src ='tarde.jpg'
+        document.body.style.background = '#b9846f'
+    } else {
+        //BOA NOITE
+        img.src = 'noite.jpg'
+        document.body.style.background = '#515154'
+    }
+}
